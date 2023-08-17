18d34bd169f96a3081b4189dd4d2f1c8d6dd7d04
0100111001101001011000110110111111 <matiaspuntocom@outlook.com>
2023-08-17 20:10:02 -0300

    trabajo recusividad actividad 3

--- src/MainFactorial.java
+++ src/MainFactorial.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
 public class MainFactorial {
 
-        public static void main(String[] args) {
-            double numero = 10;
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        // Solicita al usuario ingresar un número para calcular su factorial
+        System.out.println("ingrese el numero para sacar el factorial");
+        double numero = s.nextDouble();
 
-            // Calcula el factorial utilizando recursión
-            double factorialRecursivo = FactorialCalculator.calcularFactorial(numero);
-            System.out.println("Factorial (Recursión) de " + numero + ": " + factorialRecursivo);
+        // Calcula el factorial utilizando recursión
+        double factorialRecursivo = FactorialCalculator.calcularFactorial(numero);
+        System.out.println("Factorial (Recursión) de " + numero + ": " + factorialRecursivo);
 
-            // Calcula el factorial utilizando iteración
-            double factorialIterativo = FactorialCalculator.calcularFactorial(numero, true);
-            System.out.println("Factorial (Iteración) de " + numero + ": " + factorialIterativo);
-        }
+        // Calcula el factorial utilizando iteración
+        double factorialIterativo = FactorialCalculator.calcularFactorial(numero, true);
+        System.out.println("Factorial (Iteración) de " + numero + ": " + factorialIterativo);
     }
-
+}
--- src/SumatoriaRecursiva.java
+++ src/SumatoriaRecursiva.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class SumatoriaRecursiva {
 
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        //
+        System.out.println("ingrese el numero");
+        int numero = s.nextInt();
+
+        // Calcula la sumatoria utilizando recursión
+        int sumatoria = calcularSumatoria(numero);
+        System.out.println("La sumatoria de los números enteros desde 1 hasta " + numero + " es: " + sumatoria);
+    }
+}
     // Método recursivo para calcular la sumatoria
     public static int calcularSumatoria(int n) {
         if (n == 1) {
@@ -8,14 +21,6 @@ public class SumatoriaRecursiva {
         return n + calcularSumatoria(n - 1);
     }
 
-    public static void main(String[] args) {
-        int numero = 5;
-
-        // Calcula la sumatoria utilizando recursión
-        int sumatoria = calcularSumatoria(numero);
-        System.out.println("La sumatoria de los números enteros desde 1 hasta " + numero + " es: " + sumatoria);
-    }
-}
 
 
 
