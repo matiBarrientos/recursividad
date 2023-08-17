ed7fe6e6935ffe8afb1148d0cac1838868678e16
0100111001101001011000110110111111 <matiaspuntocom@outlook.com>
2023-08-17 20:01:17 -0300

    trabajo recusividad actividad 1

--- src/divisionConResta.java
+++ src/divisionConResta.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
+
 public class divisionConResta {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int divisor, dividendo,resultadoRecursivo,resultadoIterativo;
-        System.out.println("ingrese el dividendo");
-        dividendo = s.nextInt();
-        System.out.println("ingrese el divisor");
-        divisor = s.nextInt();
-        resultadoRecursivo = divisionRecursiva(dividendo, divisor);
-        resultadoIterativo = divisionIterativa(dividendo,divisor);
-        System.out.println("Resultado de la división con una funcion recursiva: " + resultadoRecursivo);
-        System.out.println("Resultado de la división con una funcion iterativa: " + resultadoIterativo);
-    }
-    private static int divisionIterativa (int a , int b){
-        int result=0;
-        while(a>=b){
-            a-=b;
-            result++;
+        int divisor, dividendo, resultadoRecursivo, resultadoIterativo;
         
+        // Solicita al usuario ingresar el dividendo y el divisor
+        System.out.println("Ingrese el dividendo:");
+        dividendo = s.nextInt();
+        System.out.println("Ingrese el divisor:");
+        divisor = s.nextInt();
+        
+        // Calcula los resultados utilizando funciones recursivas e iterativas
+        resultadoRecursivo = divisionRecursiva(dividendo, divisor);
+        resultadoIterativo = divisionIterativa(dividendo, divisor);
+        
+        // Muestra los resultados en la consola
+        System.out.println("Resultado de la división con una función recursiva: " + resultadoRecursivo);
+        System.out.println("Resultado de la división con una función iterativa: " + resultadoIterativo);
+    }
+    
+    // Función para realizar la división de manera iterativa
+    private static int divisionIterativa(int dividendo, int divisor) {
+        int result = 0;
+        while (dividendo >= divisor) {
+            dividendo -= divisor;
+            result++;
         }
         return result;
     }
 
+    // Función para realizar la división de manera recursiva
     public static int divisionRecursiva(int dividendo, int divisor) {
         // Caso base: si el dividendo es menor que el divisor, el resultado es 0
         if (dividendo < divisor) {
